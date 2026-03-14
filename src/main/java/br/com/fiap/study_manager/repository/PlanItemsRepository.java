eb9686a180e05e1918c632c65b0b6ebde45140c5
Anthony Souza <souzaanthony892@gmail.com>
2026-03-14 12:42:22 -0300

    ajustando lógica para inserir item no plano de estudo

--- src/main/java/br/com/fiap/study_manager/repository/PlanItemsRepository.java
+++ src/main/java/br/com/fiap/study_manager/repository/PlanItemsRepository.java
@@ -2,6 +2,7 @@ package br.com.fiap.study_manager.repository;
 
 import br.com.fiap.study_manager.models.PlanItem;
 import br.com.fiap.study_manager.models.StudyPlan;
+import br.com.fiap.study_manager.models.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import java.sql.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.sql.Types.INTEGER;
 
 @Repository
 public class PlanItemsRepository {
@@ -26,10 +29,11 @@ public class PlanItemsRepository {
 
         // Obter o objeto StudyPlan a partir do planItem
         StudyPlan studyPlan = planItem.getStudyPlan();
+        Subject subject = planItem.getSubject();
 
         String sql = "INSERT INTO DB_PLAN_ITEMS " +
-                "(id_study_plan, weekday, start_time, duration_minutes) " +
-                "VALUES (?, ?, ?, ?)";
+                "(id_study_plan, id_subject, custom_title, weekday, start_time, duration_minutes) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
 
         String[] returnColumns = { "ID" };
 
@@ -37,9 +41,11 @@ public class PlanItemsRepository {
              PreparedStatement ps = conn.prepareStatement(sql, returnColumns)) {
 
             ps.setLong(1, studyPlan.getId());
-            ps.setString(2, planItem.getWeekday());
-            ps.setTime(3, Time.valueOf(planItem.getStartTime()));
-            ps.setInt(4, planItem.getDurationMinutes());
+            ps.setNull(2, INTEGER); // Apenas para teste
+            ps.setString(3, planItem.getCustomTitle());
+            ps.setString(4, planItem.getWeekday());
+            ps.setTime(5, Time.valueOf(planItem.getStartTime()));
+            ps.setInt(6, planItem.getDurationMinutes());
 
             ps.executeUpdate();
 
@@ -56,40 +62,40 @@ public class PlanItemsRepository {
     }
 
     // Listar os itens do plano de estudo
-    public List<PlanItem> listItemsPLan(long idStudyPlan) {
-
-        // Cria a lista que vai armazenar os itens do plano de estudo
-        List<PlanItem> planItems = new ArrayList<>();
-
-        String sql = "SELECT * FROM DB_PLAN_ITEMS WHERE id_study_plan = ?";
-
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)){
-
-            // Passando para o 'where' o id do plano de estudo que vem no parâmetro
-            ps.setLong(1, idStudyPlan);
-
-            ResultSet rs = ps.executeQuery();
-
-            /* Pegando os valores dos atributos do banco
-            e passando para os atributos do java */
-            while (rs.next()) {
-                long id = rs.getLong("id");
-                String weekday = rs.getString("weekday");
-                LocalTime startTime = rs.getTime("start_time").toLocalTime();
-                Integer durationMinutes = rs.getInt("duration_minutes");
-
-                StudyPlan studyPlan = new StudyPlan(idStudyPlan);
-
-                planItems.add(
-                        new PlanItem(id, studyPlan, weekday, startTime, durationMinutes));
-            }
-
-        } catch (SQLException e) {
-            log.error("Erro ao listar tickets do Paciente.", e);
-        }
-        return planItems;
-    }
+//    public List<PlanItem> listItemsPLan(long idStudyPlan) {
+//
+//        // Cria a lista que vai armazenar os itens do plano de estudo
+//        List<PlanItem> planItems = new ArrayList<>();
+//
+//        String sql = "SELECT * FROM DB_PLAN_ITEMS WHERE id_study_plan = ?";
+//
+//        try (Connection conn = dataSource.getConnection();
+//             PreparedStatement ps = conn.prepareStatement(sql)){
+//
+//            // Passando para o 'where' o id do plano de estudo que vem no parâmetro
+//            ps.setLong(1, idStudyPlan);
+//
+//            ResultSet rs = ps.executeQuery();
+//
+//            /* Pegando os valores dos atributos do banco
+//            e passando para os atributos do java */
+//            while (rs.next()) {
+//                long id = rs.getLong("id");
+//                String weekday = rs.getString("weekday");
+//                LocalTime startTime = rs.getTime("start_time").toLocalTime();
+//                Integer durationMinutes = rs.getInt("duration_minutes");
+//
+//                StudyPlan studyPlan = new StudyPlan(idStudyPlan);
+//
+//                planItems.add(
+//                        new PlanItem(id, studyPlan, weekday, startTime, durationMinutes));
+//            }
+//
+//        } catch (SQLException e) {
+//            log.error("Erro ao listar tickets do Paciente.", e);
+//        }
+//        return planItems;
+//    }
 
     // Exclui um item do plano de estudo
     public void deletePlanItem(long id){
--- src/main/java/br/com/fiap/study_manager/services/PlanItemService.java
+++ src/main/java/br/com/fiap/study_manager/services/PlanItemService.java
@@ -1,6 +1,7 @@
 package br.com.fiap.study_manager.services;
 
 import br.com.fiap.study_manager.models.PlanItem;
+import br.com.fiap.study_manager.models.Subject;
 import br.com.fiap.study_manager.repository.PlanItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,14 @@ public class PlanItemService {
     PlanItemsRepository repository;
 
     public PlanItem addPlanItem(PlanItem planItem) {
+
+        if (planItem.getSubject() == null) {
+            Subject subject = new Subject();
+
+            subject.setId(0l);
+
+            planItem.setSubject(subject);
+        }
 
         repository.insertPlanItem(planItem);
         return planItem;
